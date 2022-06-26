f5ff643dd5c8de47be4bf8a5bc81e50c22f5510c
kongbaiming <xiariqingquan@live.cn>
2022-06-26 09:34:13 +0800

    learn java

--- src/main/java/TestStatic.java
+++ src/main/java/TestStatic.java
@@ -6,9 +6,28 @@
  * 1、从属于类，只有一份，在类被载入时被显式初始化
  * 2、一般用"类名.类属性/方法"来调用
  * 3、在static方法中不可直接访问非static的成员
+ * 构造方法用于对象初始化
+ * 静态初始化快,用于类的初始化操作
+ * 可以初始化static属性
+ * 静态初始化块中不能直接访问非static成员
  */
 public class TestStatic {
     static String company = "dev";
+    static int companyId;
+
+    /*
+       静态初始化快
+     */
+    static {
+        System.out.println("类初始化调用");
+        companyId = 123;
+        company = "king";
+        /*
+        错误调用
+        id = 10;
+        */
+    }
+
     int id;
 
     /**
@@ -21,11 +40,12 @@ public class TestStatic {
     public static void main(String[] args) {
         TestStatic.printCompany();
         TestStatic t1 = new TestStatic();
-        t1.login();
+        t1.login(19);
     }
 
-    void login() {
-        System.out.println("login...");
+    void login(int id) {
+        this.id = id;
+        System.out.println("login..." + this.id);
     }
 
 }
